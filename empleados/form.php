c5c02f33666538e051109214b779babe4d340501
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-04 18:08:03 -0500

    form
    campo rfc no está en base de datos

--- empleados/form.php
+++ empleados/form.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+		<title>Empleado</title>
+	</head>
 <?php
 require('clases/empleado.class.php');
 $empobj = new empleado;
@@ -6,7 +12,7 @@ $stt = "";
 
 if (isset($_GET['ac'])) {
 	if($_GET['ac'] == "nuevo"){
-		$form = array('nombre'=>'','rfc'=>'','correo'=>'','telefono'=>'','celular'=>'','usuario'=>'','clave'=>'','accion'=>'Registrar');
+		$form = array('nombre'=>'','rfc'=>'','empresa'=>'','correo'=>'','telefono'=>'','celular'=>'','usuario'=>'','responsable'=>'','ubicacion'=>'','puesto'=>'','clave'=>'','accion'=>'Registrar');
 	}elseif ($_GET['ac']=="editar") {
 		//obtener id
 		$form['accion']="Editar";
@@ -20,7 +26,7 @@ if (isset($_GET['ac'])) {
 if (isset($_GET['stt'])) {
 	$stt = $_GET['stt'];
 	if ($stt == "error") {
-		$msg = "No se pudo llevar a cabo la peticion deseada.";
+		$msg = "No se pudo llevar a cabo la petición deseada.";
 	}
 
 	if ($stt == "success") {
@@ -28,32 +34,63 @@ if (isset($_GET['stt'])) {
 	}
 }
 ?>
-
-<div class="<?php echo $stt; ?>"><p><?php echo $msg; ?></p></div>
-<form action="empleados/controlador.php" method="post">
-	<p>Datos personales</p>
-	<div>
-		<label>Nombre completo:</label>
-		<div><input type="text" name="nombre" id="nombre" value="<?php echo $form['nombre']; ?>"></div>
-	</div>
-	<div>
-		<label>RFC:</label>
-		<div><input type="text" name="rfc" id="rfc" pattern="^(([A-Z]|[a-z]){3,4})([0-9]{6})((([A-Z]|[a-z]|[0-9]){3}))" value="<?php echo $form['rfc']; ?>"></div>
-	</div>
-	<div>
-		<label>Correo *:</label>
-		<div><input type="email" name="correo" id="correo" value="<?php echo $form['correo']; ?>"></div>
-	</div>
-	<div>
-		<label>Telefono:</label>
-		<div><input type="text" name="telefono" id="telefono" pattern="\d{10}" value="<?php echo $form['telefono']; ?>"></div>
-	</div>
-	<div>
-		<label>Celular:</label>
-		<div><input type="text" name="celular" id="celular" pattern="\d{10}" value="<?php echo $form['celular']; ?>"></div>
-	</div>
-	<div>
-		<label></label>
-		<div style="padding-top:15px;"><input type="submit" name="a" value="<?php echo $form['accion']; ?>"></div>
-	</div>	
-</form>
+	<body>
+		<h1>Empleados</h1>
+		<div class="<?php echo $stt; ?>"><p><?php echo $msg; ?></p></div>
+		<form action="empleados/controlador.php" method="post">
+			<p>Datos personales</p>
+			<div>
+				<label>Nombre completo: </label>
+				<div><input type="text" name="nombre" id="nombre" value="<?php echo $form['nombre']; ?>" required /></div>
+			</div>
+			<div>
+				<label>RFC: </label>
+				<div><input type="text" name="rfc" id="rfc" pattern="^(([A-Z]|[a-z]){3,4})([0-9]{6})((([A-Z]|[a-z]|[0-9]){3}))" value="<?php echo $form['rfc']; ?>" required /></div>
+			</div>
+			<div>
+				<label>Empresa: </label>
+				<span>
+					<!---<select id="empresaEmp" name="empresaEmp" required></select>--->
+					<input type="text" id="empresaEmp" name="empresaEmp" required value="<?php echo $form['empresa']; ?>"/>
+				</span>
+			</div>
+			<div>
+				<label>Correo electrónico*: </label>
+				<div><input type="email" name="correo" id="correo" value="<?php echo $form['correo']; ?>" required /></div>
+			</div>
+			<div>
+				<label>Teléfono: </label>
+				<div><input type="text" name="telefono" id="telefono" pattern="\d{10}" value="<?php echo $form['telefono']; ?>" required /></div>
+			</div>
+			<div>
+				<label>Celular: </label>
+				<div><input type="text" name="celular" id="celular" pattern="\d{10}" value="<?php echo $form['celular']; ?>" required/></div>
+			</div>
+			<div>
+				<label>Responsable: </label>
+				<span>
+					<!--- <select id="responsable" name="responsable" required></select> --->
+					<input type="text" id="responsable" name="responsable" required value="<?php echo $form['responsable']; ?>"/>
+				</span>
+			</div>
+			<div>
+				<label>Ubicación: </label>
+				<span>
+					<!---<select id="ubicacion" name="ubicacion" required></select>--->
+					<input type="text" id="ubicacion" name="ubicacion" required value="<?php echo $form['ubicacion']; ?>"/>
+				</span>
+			</div>
+			<div>
+				<label>Puesto: </label>
+				<span>
+					<!---<select id="puesto" name="puesto" required></select>--->
+					<input type="text" id="puesto" name="puesto" required value="<?php echo $form['puesto']; ?>"/>
+				</span>
+			</div>
+			<div>
+				<label></label>
+				<div style="padding-top:15px;"><input type="submit" name="a" value="<?php echo $form['accion']; ?>"></div>
+			</div>	
+		</form>
+	</body>
+</html>
