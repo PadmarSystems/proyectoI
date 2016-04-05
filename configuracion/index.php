9fab97a21d18ee80436cdb08a3ecdaa867d72ccf
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-05 15:44:18 -0500

    agregar li empresa

--- configuracion/index.php
+++ configuracion/index.php
@@ -1,6 +1,7 @@
 <nav>
 	<ul>
-	    <li><a title="Empleados" href="view.php?com=empleados&mod=form&ac=nuevo">Nuevo Empleado</a></li>
-	    <li><a title="Ubicaciones" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Ubicación</a></li>
+		<li><a title="Empleados" href="view.php?com=empleados&mod=form&ac=nuevo">Nuevo Empleado</a></li>
+		<li><a title="Ubicaciones" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Ubicación</a></li>
+		<li><a title="Empresa" href="view.php?com=empresa&mod=form&ac=editar">Mi Empresa</a></li>
 	</ul>
 </nav>
