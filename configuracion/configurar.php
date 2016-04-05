5af7aad96060b5023efb8f81f20d621e79d63ab2
nestor <nestor.manuel@padmarsystems.com>
2016-04-05 13:07:45 -0500

    act opciones config

--- configuracion/configurar.php
+++ configuracion/configurar.php
@@ -2,5 +2,9 @@
 	<ul>
 	    <li><a title="Empleados" href="view.php?com=empleados&mod=form&ac=nuevo">Nuevo Empleado</a></li>
 	    <li><a title="Ubicaciones" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Ubicación</a></li>
+	    <li><a title="Empresas" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Empresa</a></li>
+	    <li><a title="Usuarios" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nuevo Usuario</a></li>
+	    <li><a title="Respnsables" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nuevo Responsable</a></li>
+	    <li><a title="Puestos" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nuevo Puesto</a></li>
 	</ul>
 </nav>
