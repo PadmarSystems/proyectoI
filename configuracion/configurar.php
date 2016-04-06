0f455c75ab21376b6a2ed7b3aab8a7235daf0520
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-06 18:05:57 -0500

    Update and rename index.php to configurar.php

--- configuracion/configurar.php
+++ configuracion/configurar.php
@@ -1,7 +1,8 @@
 <nav>
 	<ul>
-		<li><a title="Empleados" href="view.php?com=empleados&mod=form&ac=nuevo">Nuevo Empleado</a></li>
-		<li><a title="Ubicaciones" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Ubicación</a></li>
 		<li><a title="Empresa" href="view.php?com=empresa&mod=form&ac=editar">Mi Empresa</a></li>
+		<li><a title="Empleados" href="view.php?com=empleados&mod=form&ac=nuevo">Nuevo Empleado</a></li>
+		<li><a title="Puestos" href="view.php?com=puestos&mod=form&ac=nuevo">Nuevo Puesto</a></li>
+		<li><a title="Ubicaciones" href="view.php?com=ubicaciones&mod=form&ac=nuevo">Nueva Ubicación</a></li>
 	</ul>
 </nav>
