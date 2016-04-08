96b6eb173faf678196aba5d9076acfcbd00d3e74
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-08 15:04:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+$msg = "Favor de ingresar sus datos";
+$stt = "";
+
+if(isset($_SESSION)){
+	header('Location: view.php?');
+	exit;
+}
+
+if (isset($_GET['stt'])) {
+	$stt = $_GET['stt'];
+	if ($stt == "error") {
+		$msg = "Compruebe que los datos introducidos sean correctos";
+	}
+
+	if ($stt == "ended") {
+		$msg="Sesión finalizada. Hasta pronto";
+	}
+}
 require("ruta.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -10,7 +28,7 @@ require("ruta.php");
 </head>
 <body>
     <form action="view.php" method="post">
-        <p>Favor de ingresar sus datos</p>
+        <p><?php echo $msg; ?></p>
         <div><span><i class="fa fa-user"></i></span><input type="text" placeholder="Usuario" value="" autofocus id="myusername" name="myusername" required/></div>
     	<div><span><i class="fa fa-lock"></i></span><input type="password" placeholder="Contraseña" value="" id="mypassword" name="mypassword" required/></div>
         <input type="submit" value="Iniciar Sesión" />
