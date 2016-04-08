f42404cf9210f27512aeff542bd168d2e9618322
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-08 15:08:41 -0500

    Update portada.php

--- portada.php
+++ portada.php
@@ -1,1 +1,8 @@
-<p>Demo</p>
+<nav>
+  <ul>
+    <li><a title="Empleados" onclick="goto('listar','empleados');">Ver empleados</a></li>
+    <li><a title="Incidencias" onclick="goto('','incidencias')">Nueva Incidencia</a></li>
+    <li><a title="Reportes" onclick="goto('','reportes')">Generar Reporte</a></li>
+    <li><a title="Configs" onclick="goto('configurar','configuracion')">Configuración</a></li>
+  </ul>
+</nav>
