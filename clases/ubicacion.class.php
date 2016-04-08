df7ba39e7fa6d480138b68d4e798220677e74d47
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-08 10:24:42 -0500

    corr funciones

--- clases/ubicacion.class.php
+++ clases/ubicacion.class.php
@@ -4,26 +4,25 @@ require_once 'config.gral.php';
 require_once 'safemysql.php';
 
 class ubicaciones extends SafeMySQL {
-    private $keyHash = 'sad.SD$24';//clave hashing para encriptar
     var $con;
 
     function __construct() {
         $this->con = new SafeMySQL();
     }
 
-	function getUbicacion($datos){
-		return $this->con->getRow("SELECT * FROM `ubicacion` WHERE idEmpresa= ? ORDER BY `nombreUbicacion` ASC");
+	function actualizarUbicacion($name,$id){
+		$result = $this->con->query("UPDATE `ubicacion` SET `nombreUbicacion` = ?s WHERE `idUbicacion` = ?i",$name,$id);
+		if($result) return true;
 	}
-	function nuevaUbicacion($datos){
+	function getEmpleadosxUbic($id){
+		return $this->con->getAll("SELECT COUNT(*) FROM empleado WHERE idUbicacion=?i",$id);
+	}
+	function getUbicacionesxEmp($datos){
+		return $this->con->getAll("SELECT * FROM `ubicacion` WHERE idEmpresa= ?i ORDER BY `nombreUbicacion` ASC",$datos);
+	}
+	function insertarUbicacion($datos){
 		$result = $this->con->query("INSERT INTO `ubicacion` SET ?u", $datos);
 		if($result) return true;
 	}
-
-	function actualizarUbicacion($datos=array()){
-		//func tiene array como arg
-		$result = $this->con->query("UPDATE `ubicacion` SET `nombreUbicacion` = ? WHERE `idUbicacion` = ?");
-		if($result) return true;
-	}
-
 }
 ?>
