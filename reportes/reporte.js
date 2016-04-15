38d9ba417da3b335daacfc2eeaf6b649307678c4
nestor <nestor.manuel@padmarsystems.com>
2016-04-15 18:21:33 -0500

    correccion tabla reporte

--- reportes/reporte.js
+++ reportes/reporte.js
@@ -23,7 +23,41 @@ function filtrar_incidencias(empresa,ubicacion,responsable,puesto,tipoIn,fechai,
         },
         async: true,
         success: function (msg) {
+        	$(".listado").dataTable().fnDestroy();
             $("#tabla").empty().append(msg);
+            $(".listado").dataTable({
+                "dom": '<lf>rt<ip>',
+                stateSave: true,
+                aLengthMenu: [
+                    [25, 10, 50, 100, -1],
+                    [25, 10, 50, 100, "Todo"]
+                ],
+                "bSort": true,
+                "language": {
+                    "emptyTable": "No hay datos disponibles en la tabla.",
+                    "info": "Se muestran de _START_ a _END_ de _TOTAL_",
+                    "infoEmpty": "Se muestran de 0 a 0 de 0",
+                    "infoFiltered": "(filtrado de _MAX_ totales)",
+                    "infoPostFix": "",
+                    "thousands": ",",
+                    "lengthMenu": "Mostrar _MENU_",
+                    "loadingRecords": "Cargando...",
+                    "processing": "Procesando...",
+                    "search": "Buscar:",
+                    "zeroRecords": "No se encontraron resultados.",
+                    "paginate": {
+                        "first": "Primera",
+                        "last": "Última",
+                        "next": "Sig.",
+                        "previous": "Ant."
+                    },
+                    "aria": {
+                        "sortAscending":  ": activar para ordenar ascendente",
+                        "sortDescending": ": activar para ordenar descendente"
+                    }
+                },
+                "pagingType": "full_numbers"
+            });
         }
     });
 }
