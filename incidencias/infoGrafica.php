179da7dcf2a7404291adea762e7a55a24db8c078
mihm-rmk <monihm.rmk02@hotmail.com>
2016-04-26 16:24:26 -0500

    Update infoGrafica.php
    cambiar alert() por dialog (contenido).

--- incidencias/infoGrafica.php
+++ incidencias/infoGrafica.php
@@ -30,42 +30,29 @@ switch ($bandera){
 	case 2:
 		$tipo = $_POST['incidencia'];
 		$listaIncid = $incidencia->mostrar_incidenciasfiltro('SELECT * FROM incidencias WHERE idTipoIncidencia='.$tipo.' AND idEmpleado='.$id);
-		$Ene = 0;  $Feb = 0;  $Mar = 0;
-		$Abr = 0;  $May = 0;  $Jun = 0;
-		$Jul = 0;  $Ago = 0;  $Sep = 0;
-		$Oct = 0;  $Nov = 0;  $Dic = 0;
+		$Ene = 0;  $Feb = 0;  $Mar = 0;  $Abr = 0;
+		$May = 0;  $Jun = 0;  $Jul = 0;  $Ago = 0;
+		$Sep = 0;  $Oct = 0;  $Nov = 0;  $Dic = 0;
 		foreach ($listaIncid as $fechas){
 			$date = explode(' ',$fechas['fechaInicio']);
 			$date = explode('-',$date[0]);
 			switch($date[1]){
-				case '01': $Ene++;
-					break;
-				case '02': $Feb++;
-					break;
-				case '03': $Mar++;
-					break;
-				case '04': $Abr++;
-					break;
-				case '05': $May++;
-					break;
-				case '06': $Jun++;
-					break;
-				case '07': $Jul++;
-					break;
-				case '08': $Ago++;
-					break;
-				case '09': $Sep++;
-					break;
-				case '10': $Oct++;
-					break;
-				case '11': $Nov++;
-					break;
-				case '12': $Dic++;
-					break;
+				case '01': $Ene++; break;
+				case '02': $Feb++; break;
+				case '03': $Mar++; break;
+				case '04': $Abr++; break;
+				case '05': $May++; break;
+				case '06': $Jun++; break;
+				case '07': $Jul++; break;
+				case '08': $Ago++; break;
+				case '09': $Sep++; break;
+				case '10': $Oct++; break;
+				case '11': $Nov++; break;
+				case '12': $Dic++; break;
 			}
 		}
 		$arreglo = array($Ene,$Feb,$Mar,$Abr,$May,$Jun,$Jul,$Ago,$Sep,$Oct,$Nov,$Dic);
 		echo json_encode($arreglo);
 	break;
-	
+}
 ?>
