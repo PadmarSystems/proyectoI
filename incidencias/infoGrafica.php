5503b33871812902db141e6d1ebf8f3da08a9f2b
mihm-rmk <monihm.rmk02@hotmail.com>
2016-05-24 14:21:58 -0500

    corrección graficos

--- incidencias/infoGrafica.php
+++ incidencias/infoGrafica.php
@@ -5,21 +5,86 @@ require('../clases/incidencia.class.php');
 $incidencia = new incidencia;
 $empleados = $empleado->mostrar_empleados();
 $tiposincidencias = $incidencia->mostrar_tipo_incidencias();
-
-$id=$_POST['empleado'];
-$empresa=$_POST['empresa'];
+$yr = date('Y');
+	$yr = 2015;
 $bandera = $_POST['bandera'];
+#if ($bandera != '3punto1'){
+	$id=$_POST['empleado'];
+	$empresa=$_POST['empresa'];
+#}
+#print_r($_POST);
 switch ($bandera){
 	case 1:
-		$date=$_POST['fecha'];
-		if($date<10){
-			$date = "0".$date;
+		$emp = $incidencia->mostrar_incidenciasfiltro("SELECT DISTINCT idEmpleado FROM incidencias WHERE idTipoIncidencia=".$_POST['incidencia'].' AND idEmpresa='.$empresa.' ORDER BY idIncidencia');
+		if (!empty($emp)){
+			foreach($emp as $val){
+				$lista=$incidencia->mostrar_incidenciasfiltro("SELECT idEmpleado,nombreEmp FROM empleados WHERE idEmpleado=".$val['idEmpleado']." AND idEmpresa=".$empresa);
+				if (!empty($lista)){
+					foreach ($lista as $val){
+						$nombre = str_replace('--',' ',$val['nombreEmp']);
+						$tst[] = array('nombre'=>$nombre,'id'=>$val['idEmpleado']);
+					}
+				} else {
+					$tst=0;
+				}
+			}
+		} else {
+			$tst = 0;
 		}
-		$listaIncid = $incidencia->mostrar_incidenciasfiltro('SELECT * FROM tipo_incidencia');
+		if ($tst != 0){
+			#print_r($tst);
+			foreach ($tst as $ts){
+				$nmE = $ts['nombre'];
+				$idE = $ts['id'];
+				
+				$data1=array(); 
+				$listaIncid=$incidencia->mostrar_incidenciasfiltro("SELECT idEmpleado,idTipoIncidencia,fechaInicio FROM incidencias WHERE idTipoIncidencia = ".$_POST['incidencia']." AND idEmpleado=".$idE." ORDER BY fechaInicio");
+				$res=0;
+				foreach ($listaIncid as $val){
+					$fecha=explode(' ',$val['fechaInicio']);
+					/*
+					$fecha=explode('-',$fecha[0]);
+					$tiempo = mktime($hora[0]-6,$hora[1],$hora[2],$fecha[1],$fecha[2],$fecha[0]);
+					*/
+					$a = (double)$idE;
+					$res++;
+					$data1[] = array($fecha[0],$res,$idE);
+				}
+				$aux=[];
+				foreach ($data1 as $key => $row) {
+					$aux[$key] = $row[0];
+				}
+				$cuenta = count($data1);	
+				array_multisort($aux,SORT_ASC,$data1);
+				$arregloDatos[] = array('name'=>$nmE,'data'=>$data1,'NUM'=>$cuenta);
+			}
+		}
+		echo json_encode($arregloDatos);
+			 /*foreach ($listaEmp as $emp){
+				$datos11 = array();
+				$nm = str_replace('--',' ',$emp['nombreEmp']);
+				$datos[]=array('name'=>$nm);
+				foreach ($idIncid as $incid){
+					$listaIncid = $incidencia->verIncidenciasEmpTipo($emp['idEmpleado'],$incid['idTipo'],$yr,$mes);
+					$datos11[]=array('incidencia'=>$incid['tipoIncidencia'],'num'=>$listaIncid);
+				}
+				$datos[]=array($datos11);
+			}
+		} else {
+			$datos=0;
+		}
+		echo json_encode($datos); */
+	
+	break;
+	case 2:
+		$mes=$_POST['fecha'];
+		if($mes<10){
+			$mes = "0".$mes;
+		}
+		$listaIncid = $incidencia->mostrar_tipo_incidencias();
 		$arreglo = array();
 		foreach ($listaIncid as $inc) {
-			$noIncid= $incidencia->verIncidenciasTipoFecha($id,$inc['idTipo'],$date);
-			$noIncid = count($noIncid);
+			$noIncid= $incidencia->verIncidenciasEmpTipo($id,$inc['idTipo'],$yr,$mes);
 			if ($noIncid == 0){
 				continue;
 			}
@@ -27,9 +92,46 @@ switch ($bandera){
 		}
 		echo json_encode($arreglo);
 	break;
-	case 2:
+	case 3:
 		$tipo = $_POST['incidencia'];
-		$listaIncid = $incidencia->mostrar_incidenciasfiltro('SELECT * FROM incidencias WHERE idTipoIncidencia='.$tipo.' AND idEmpleado='.$id);
+		$mes = $_POST['fecha'];
+		switch ($mes){
+			case 1:
+				$date = 'Enero';
+				$yr = $yr-1;
+				$yr=2015;
+			break;
+			case 2: $date = 'Febrero'; break;
+			case 3: $date = 'Marzo'; break;
+			case 4: $date = 'Abril'; break;
+			case 5: $date = 'Mayo'; break;
+			case 6: $date = 'Junio'; break;
+			case 7: $date = 'Julio'; break;
+			case 8: $date = 'Agosto'; break;
+			case 9: $date = 'Septiembre'; break;
+			case 10: $date = 'Octubre'; break;
+			case 11: $date = 'Noviembre'; break;
+			case 12: $date = 'Diciembre'; break;
+		}
+		$date = $date.' '.$yr;
+		if($mes<10){
+			$mes = "0".$mes;
+		}
+		$listaEmp = $incidencia->mostrar_incidenciasfiltro('SELECT idEmpleado,nombreEmp FROM empleados WHERE idEmpresa='.$empresa);
+		foreach ($listaEmp as $emp){
+			$listaIncid = $incidencia->verIncidenciasEmpTipo($emp['idEmpleado'],$tipo,$yr,$mes);
+			$nm = str_replace('--',' ',$emp['nombreEmp']);
+			if (!empty($listaIncid)){
+				$datos[] = array('name'=>$nm,'num'=>$listaIncid,'date'=>$date);
+			} else {
+				$datos[] = array('name'=>$nm,'num'=>0,'date'=>$date);
+			}
+		}
+		echo json_encode($datos);
+	break;
+	case 4:
+		$tipo = $_POST['incidencia'];
+		$listaIncid = $incidencia->mostrar_incidenciasfiltro('SELECT * FROM incidencias WHERE idTipoIncidencia='.$tipo.' AND idEmpleado='.$id.' AND fechaInicio like "'.$yr.'-%"');
 		$Ene = 0;  $Feb = 0;  $Mar = 0;  $Abr = 0;
 		$May = 0;  $Jun = 0;  $Jul = 0;  $Ago = 0;
 		$Sep = 0;  $Oct = 0;  $Nov = 0;  $Dic = 0;
@@ -54,5 +156,53 @@ switch ($bandera){
 		$arreglo = array($Ene,$Feb,$Mar,$Abr,$May,$Jun,$Jul,$Ago,$Sep,$Oct,$Nov,$Dic);
 		echo json_encode($arreglo);
 	break;
-}
+	case 5:
+		$idResp = $_POST['respOubic'];
+		$mes=$_POST['fecha'];
+		if($mes<10){
+			$mes = "0".$mes;
+		}
+		$listaEmp=$incidencia->mostrar_incidenciasfiltro("SELECT idEmpleado,nombreEmp FROM empleados WHERE idResponsable=".$idResp." AND idEmpresa=".$empresa);
+		$idIncid = $incidencia -> mostrar_tipo_incidencias();
+		if (!empty($listaEmp)){
+			foreach ($listaEmp as $emp){
+				$datos11 = array();
+				$nm = str_replace('--',' ',$emp['nombreEmp']);
+				$datos[]=array('name'=>$nm);
+				foreach ($idIncid as $incid){
+					$listaIncid = $incidencia->verIncidenciasEmpTipo($emp['idEmpleado'],$incid['idTipo'],$yr,$mes);
+					$datos11[]=array('incidencia'=>$incid['tipoIncidencia'],'num'=>$listaIncid);
+				}
+				$datos[]=array($datos11);
+			}
+		} else {
+			$datos=0;
+		}
+	echo json_encode($datos);
+	break;
+	case 6:
+		$idUbic = $_POST['respOubic'];
+		$mes=$_POST['fecha'];
+		if($mes<10){
+			$mes = "0".$mes;
+		}
+		$listaEmp=$incidencia->mostrar_incidenciasfiltro("SELECT idEmpleado,nombreEmp FROM empleados WHERE idUbicacion=".$idUbic." AND idEmpresa=".$empresa);
+		$idIncid = $incidencia -> mostrar_tipo_incidencias();
+		if (!empty($listaEmp)){
+			foreach ($listaEmp as $emp){
+				$datos11 = array();
+				$nm = str_replace('--',' ',$emp['nombreEmp']);
+				$datos[]=array('name'=>$nm);
+				foreach ($idIncid as $incid){
+					$listaIncid = $incidencia->verIncidenciasEmpTipo($emp['idEmpleado'],$incid['idTipo'],$yr,$mes);
+					$datos11[]=array('incidencia'=>$incid['tipoIncidencia'],'num'=>$listaIncid);
+				}
+				$datos[]=array($datos11);
+			}
+		} else {
+			$datos=0;
+		}
+		echo json_encode($datos);
+	break;
+	}
 ?>
